5681c09164787c2cab9ea14e3a62c33af28c8943
seven <2805426177@qq.com>
2024-09-01 08:42:03 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ app/src/main/java/com/example/soundworld/LoginFragment.java
@@ -0,0 +1,19 @@
+package com.example.soundworld;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.soundworld.R;
+
+public class LoginFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frag_login, container, false);
+        return view;
+    }
+}
